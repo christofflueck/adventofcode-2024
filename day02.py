eea429dafd3a30a2017d9762cda2c41d37fc57e7
Christof Flück <christof.flueck@comfone.com>
2024-12-02 15:00:43 +0100

    Finished Part B. Not Very clean but works

--- day02.py
+++ day02.py
@@ -1,8 +1,27 @@
+from typing import List
+
 from run_util import run_puzzle
 
 
-def parse_data(data: str):
+def parse_data(data: str) -> List[List[int]]:
     return [[int(x) for x in row.split()] for row in data.split('\n')]
+
+
+def is_row_safe(row: List[int]) -> (bool, int):
+    increasing = True
+    prev = None
+    for i, num in enumerate(row):
+        if prev is None:
+            if row[0] > row[1]:
+                increasing = False
+            if row[0] == row[1]:
+                return False, 1
+            prev = num
+            continue
+        if prev == num or (prev > num and increasing) or (prev < num and not increasing) or 1 < abs(prev - num) > 3:
+            return False, i
+        prev = num
+    return True, None
 
 
 def part_a(data: str) -> int:
@@ -10,25 +29,8 @@ def part_a(data: str) -> int:
     safe = 0
 
     for row in data:
-        increasing = True
-        is_unsafe = False
-        prev = None
-        for num in row:
-            if prev is None:
-                if row[0] > row[1]:
-                    increasing = False
-                if row[0] == row[1]:
-                    is_unsafe = True
-                    break
-                prev = num
-                continue
-
-            if prev == num or (prev > num and increasing) or (prev < num and not increasing) or 1 < abs(prev - num) > 3:
-                is_unsafe = True
-                break
-            prev = num
-
-        if not is_unsafe:
+        row_safe, i = is_row_safe(row)
+        if row_safe:
             safe = safe + 1
 
     return safe
@@ -36,7 +38,20 @@ def part_a(data: str) -> int:
 
 def part_b(data: str) -> int:
     data = parse_data(data)
-    return 0
+    safe = 0
+
+    for row in data:
+        row_safe, i = is_row_safe(row)
+        if row_safe:
+            safe = safe + 1
+        else:
+            for i in range(len(row)):
+                new_row = row[:i] + row[i + 1:]
+                row_safe, i = is_row_safe(new_row)
+                if row_safe:
+                    safe = safe + 1
+                    break
+    return safe
 
 
 def main():
